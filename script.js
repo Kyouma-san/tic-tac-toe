43fb64fd1ed415439e4ddd1da2a9b57225c7c885
kyouma_san <rohananand56@gmail.com>
2022-01-25 03:59:55 +0530

    finishing project

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let turn = "X"
 let gameover =false;
+let width = screen.width;
 //Function to change player turn
 
 const changeTurn = () => {
@@ -9,7 +10,9 @@ const changeTurn = () => {
 //function to cehck winning condition
 
 const checkWin = () => {
-    let boxetext = document.getElementsByClassName("boxtext");
+    let boxtext = document.getElementsByClassName("boxtext");
+    let box = document.getElementsByClassName("box");
+
     let wins = [
         [0, 1, 2],
         [3, 4, 5],
@@ -21,15 +24,32 @@ const checkWin = () => {
         [2, 4, 6]
     ]
 
-    wins.forEach(e => {
-        if ((boxetext[e[0]].innerText === boxetext[e[1]].innerText) &&
-            (boxetext[e[1]].innerText === boxetext[e[2]].innerText) &&
-            boxetext[e[1]].innerText !== "") {
-                document.querySelector('.info').innerText = "Player " + (turn == 'X' ? 1 : 2) + " WON!!!!"
-                gameover = true;
-                document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-        }
-    })
+    if(gameover == false){
+        wins.forEach(e => {
+            if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) &&
+                (boxtext[e[1]].innerText === boxtext[e[2]].innerText) &&
+                boxtext[e[1]].innerText !== "") {
+                    document.querySelector('.info').innerText = "Player " + (turn == 'X' ? 1 : 2) + " WON!!!!"
+                    gameover = true;
+                    if(width>1200){
+                        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+                    } else if(width > 1020){
+                        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "120px";
+                    }
+                    else if(width > 800){
+                        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "81px";
+                    } else {
+                        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "81px";
+                    }
+                    
+                    box[e[1]].style.backgroundColor = "#f38068"
+                    box[e[0]].style.backgroundColor = "#f38068"
+                    box[e[2]].style.backgroundColor = "#f38068"
+            }
+        })
+    }
+
+    
 }
 
 //game logic
@@ -51,9 +71,46 @@ Array.from(boxes).forEach(element => {
     element.addEventListener('touchend', () => {
         if (boxtext.innerText === '') {
             boxtext.innerText = turn;
-            turn = changeTurn();
             checkWin();
-            document.getElementsByClassName("info")[0].innerText = "Player " + (turn == 'X' ? 1 : 2) + " turn";
+            turn = changeTurn();
+            if(!gameover){
+                document.getElementsByClassName("info")[0].innerText = "Player " + (turn == 'X' ? 1 : 2) + " turn";
+            }
         }
+    })
+})
+
+
+
+
+reset.addEventListener('click',()=>{
+    
+    let boxtext = document.querySelectorAll('.boxtext');
+    Array.from(boxtext).forEach(element=>{
+        element.innerText=""
+    })
+    turn = "X";
+    document.getElementsByClassName("info")[0].innerText = "Player " + (turn == 'X' ? 1 : 2) + " turn";
+    gameover = false;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+    let boxes = document.getElementsByClassName("box");
+    Array.from(boxes).forEach(element => {
+        element.style.backgroundColor = "transparent"
+    })
+})
+
+reset.addEventListener('touchend',()=>{
+    
+    let boxtext = document.querySelectorAll('.boxtext');
+    Array.from(boxtext).forEach(element=>{
+        element.innerText=""
+    })
+    turn = "X";
+    document.getElementsByClassName("info")[0].innerText = "Player " + (turn == 'X' ? 1 : 2) + " turn";
+    gameover = false;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+    let boxes = document.getElementsByClassName("box");
+    Array.from(boxes).forEach(element => {
+        element.style.backgroundColor = "transparent"
     })
 })
